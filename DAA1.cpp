301d0f7536687812b3adf57704c1b043ecb85136
Shivam Joshi <95294234+ShivamJoshi9@users.noreply.github.com>
2022-04-08 19:32:51 +0530

    Update DAA1.cpp

--- DAA1.cpp
+++ DAA1.cpp
@@ -1,3 +1,19 @@
+1. Given an unsorted array of integers, design an algorithm and implement it using a program to
+sort an array of elements by dividing the array into two subarrays and combining these subarrays
+after sorting each one of them. Your program should also find number of comparisons and
+inversions during sorting the array.
+Input Format: 
+The first line contains number of test cases, T.
+For each test case, there will be two input lines.
+First line contains n (the size of array).
+Second line contains space-separated integers describing array.
+Output Format:
+The output will have T number of lines. 
+For each test case T, there will be three output lines.
+First line will give the sorted array.
+Second line will give total number of comparisons.
+Third line will give total number of inversions required.
+
 #include <iostream>
 using namespace std;
 
